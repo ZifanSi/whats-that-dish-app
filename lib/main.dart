88c7bffc1580ed9aa4d294a640d6817ab4ec8072
Zifan Si <siz@mcmaster.ca>
2025-03-24 23:20:21 -0400

    Add more pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
+import 'screens/login_page.dart';
+import 'screens/experts_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,73 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Login Demo', home: LoginPage());
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  String _message = '';
-
-  Future<List<Map<String, dynamic>>> _loadUsers() async {
-    final jsonString = await rootBundle.loadString('assets/data/t_users.json');
-    final List<dynamic> jsonList = json.decode(jsonString);
-    return jsonList.cast<Map<String, dynamic>>();
-  }
-
-  Future<void> _login() async {
-    final email = _emailController.text.trim();
-    final password = _passwordController.text;
-
-    final users = await _loadUsers();
-
-    final user = users.firstWhere(
-      (u) => u['email'] == email && u['password'] == password,
-      orElse: () => {},
-    );
-
-    setState(() {
-      if (user.isNotEmpty) {
-        _message = 'Login successful!';
-      } else {
-        _message = 'Invalid email or password.';
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Password'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(onPressed: _login, child: const Text('Login')),
-            const SizedBox(height: 20),
-            Text(_message, style: const TextStyle(fontSize: 16)),
-          ],
-        ),
-      ),
+    return MaterialApp(
+      title: 'Login Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginPage(),
+        '/experts': (context) => const ExpertsPage(),
+      },
     );
   }
 }
